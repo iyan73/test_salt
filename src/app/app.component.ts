1fc2704080907bd3d903c4fa1146f1ccd160bb62
iyan73 <iyan732015@gmail.com>
2020-11-14 16:41:51 +0700

    update sore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'test-salt';
+  title = "test-salt";
+
+  listActivities = [
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    },
+    {
+      image: "https://material.angular.io/assets/img/examples/shiba1.jpg",
+      subtitle: "Subtitle",
+      title: "title"
+    }
+  ];
 }
